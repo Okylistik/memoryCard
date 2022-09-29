16edb409d9f731b8b9260f7713d78d3b014f9c42
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-09-29 17:46:57 +0300

    ready!

--- app.js
+++ app.js
@@ -2,28 +2,19 @@
 
 const ulList = document.querySelector(".game-box");
 const div = document.createElement("div");
+const banner = document.querySelector(".result");
+
 const imgBack = document.createElement("img");
 imgBack.src = "./img/back.jpg";
-console.log(ulList);
 
 let cards = [1, 2, 3, 4, 5, 6];
 cards = [...cards, ...cards];
-
-// перемешать
-function random() {
-  let j, temp;
-  cards.forEach((i) => {
-    j = Math.floor(Math.random() * cards.length);
-    temp = cards[j];
-    cards[j] = cards[i];
-    cards[i] = temp;
-  });
-}
+let pairCards = [];
 
 const generateCard = (num, id) => {
   const card = `
   <li class="game-item">
-<div class="flipper" data-id=${id}>
+<div class="flipper" data-item=${id}>
     <div class="front">
         <img src="./img/${num}.jpg" alt="">
     </div>
@@ -36,70 +27,90 @@ const generateCard = (num, id) => {
   return card;
 };
 
-function reset() {
-  while (ulList.firstChild) {
-    ulList.removeChild(ulList.firstChild);
-  }
-}
-
-// вывод 12 карт
 const startGame = () => {
+  banner.classList.add("popup");
   random();
   reset();
   let id = 0;
   const cardsOnBoard = (i) =>
     ulList.insertAdjacentHTML("beforeend", generateCard(i, id++));
   cards.forEach((card) => cardsOnBoard(card));
-  console.log(cards);
 };
+
+startGame();
+
+ulList.addEventListener("click", onClick);
+
+function reset() {
+  while (ulList.firstChild) {
+    ulList.removeChild(ulList.firstChild);
+  }
+}
+
+function random() {
+  let j, temp;
+  cards.forEach((i) => {
+    j = Math.floor(Math.random() * cards.length);
+    temp = cards[j];
+    cards[j] = cards[i];
+    cards[i] = temp;
+  });
+}
 
 function checkCards(pathCard) {
   return pathCard.querySelector(".front").img;
 }
 
-//
-let pairCards = [];
-// клик переворот
-ulList.addEventListener("click", (e) => {
-  const thisCard = e.target.closest(".flipper");
-  thisCard.classList.add("flip");
-  const idCard = thisCard.querySelector(".front");
-  // idCard.children
-  pairCards.push(thisCard.dataset.id);
-  console.log(e.target);
-  console.log(pairCards);
-  if (pairCards.length === 2) {
-    //reset
-    sameCard(pairCards[0], pairCards[1]);
-    setTimeout(() => {
-      clothedCard();
-    }, 1000);
-    return (pairCards = []);
+function onClick(e) {
+  if (openedCard().length < 2) {
+    const thisCard = e.target.closest(".flipper");
+    thisCard.classList.add("flip");
+
+    if (thisCard.dataset.item !== pairCards[0]) {
+      pairCards.push(thisCard.dataset.item);
+    }
+
+    if (pairCards.length >= 2) {
+      sameCard(pairCards[0], pairCards[1]);
+      return (pairCards = []);
+    }
   }
-});
-console.log(sameCard());
+}
 
-// синхронизация картинок
-// thisCard
+function openedCard() {
+  return ulList.querySelectorAll(".flip");
+}
 
-// закрывается карта
 function clothedCard() {
-  const openedCard = ulList.querySelectorAll(".flip");
-  openedCard.forEach((i) => i.classList.remove("flip"));
+  openedCard().forEach((i) => i.classList.remove("flip"));
 }
 
-// найдены одинаковые
 function sameCard(first, second) {
-  console.log(cards[second]);
-  console.log(cards[first]);
+  const pData = document.getElementsByTagName("dataset.id");
+
   if (cards[first] === cards[second]) {
-    // удаляются
-    console.log("должны удаляться");
+    delay(killPair);
   } else {
-    // clothedCard ()
-    console.log("закрываются");
+    delay(clothedCard);
   }
 }
-//
 
-// два клика максимум
+function delay(fn, time = 1000) {
+  return setTimeout(() => fn(), time);
+}
+
+function killPair() {
+  openedCard().forEach((card) => {
+    card.classList.add("hide");
+    card.classList.remove("flip");
+  });
+  emptyFlow();
+}
+
+function emptyFlow() {
+  const hideListCards = document.querySelectorAll(".hide");
+  if (hideListCards.length === 12) {
+    reset();
+    return banner.classList.remove("popup");
+  }
+}
